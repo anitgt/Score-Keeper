6da8d47516d657d0c14892a5245b266f2e14c2c8
Anit <anit7795@gmail.com>
2025-06-22 00:11:35 +0530

    Game over logic

--- app.js
+++ app.js
@@ -6,17 +6,28 @@ const p2Display = document.querySelector('#p2Display');
 let p1Score = 0;
 let p2Score = 0;
 let WinningScore = 5;
+let isGameOver = false;
 
 p1Button.addEventListener('click', () => {
-    if(p1Score !== WinningScore) {
-         p1Score += 1;
+    if(!isGameOver) {
+        if(p1Score !== WinningScore) {
+        p1Score += 1;
+            if(p1Score === WinningScore) {
+            isGameOver = true;
+            }
         p1Display.innerText = p1Score;
-    } 
+} 
+    }
 })
 
 p2Button.addEventListener('click', () => {
-     if(p2Score !== WinningScore) {
-         p2Score += 1;
+    if(!isGameOver) {
+        if(p2Score !== WinningScore) {
+        p2Score += 1;
+            if(p2Score === WinningScore) {
+                isGameOver = true;
+            }
         p2Display.innerText = p2Score;
+}
     }
 })
