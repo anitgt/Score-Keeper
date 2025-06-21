d0d21c35aa62917f9bb13779ea99fa5e4608b87b
Anit <anit7795@gmail.com>
2025-06-22 00:40:03 +0530

    winner loser color

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ p1Button.addEventListener('click', () => {
         p1Score += 1;
             if(p1Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
             }
         p1Display.innerText = p1Score;
 } 
@@ -28,6 +30,8 @@ p2Button.addEventListener('click', () => {
         p2Score += 1;
             if(p2Score === winningScore) {
                 isGameOver = true;
+                p1Display.classList.add('loser')
+                p2Display.classList.add('winner')
             }
         p2Display.innerText = p2Score;
 }
@@ -50,4 +54,6 @@ resetButton.addEventListener('click', reset)
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
+    p1Display.classList.remove('winner','loser')
+    p2Display.classList.remove('winner', 'loser')
 }
